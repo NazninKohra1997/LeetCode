8c3edbef99540c6e8305a515c84e55be98f1ae4c
NazninKohra1997 <jhora.eceiu@gmail.com>
2023-08-16 13:16:35 +0600

    I added some new files

---
+++ leetcode/two_sum/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        // Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        int[] arr = new int[2];
+
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int sum = 0;
+                sum = nums[i] + nums[j];
+                if (sum == target)
+                {
+                    arr[0] = i;
+                    arr[1] = j;
+                    break;
+                }
+            }
+        }
+
+        return arr;
+
+    }
+}
